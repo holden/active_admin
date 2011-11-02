96cbd1c60f431a1a41509ded17a354316999aaab
Holden Thomas <holden.thomas@gmail.com>
2011-11-02 13:08:34 +0100

    try this

--- lib/active_admin/views/components/table_for.rb
+++ lib/active_admin/views/components/table_for.rb
@@ -6,6 +6,26 @@ module ActiveAdmin
       def tag_name
         'table'
       end
+      
+      # Adds links to View, Edit and Delete
+      def default_actions(options = {})
+        options = {
+          :name => ""
+        }.merge(options)
+        column options[:name] do |resource|
+          links = ''.html_safe
+          if controller.action_methods.include?('show')
+            links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
+          end
+          if controller.action_methods.include?('edit')
+            links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
+          end
+          if controller.action_methods.include?('destroy')
+            links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
+          end
+          links
+        end
+      end
 
       def build(collection, options = {})
         @sortable = options.delete(:sortable)
